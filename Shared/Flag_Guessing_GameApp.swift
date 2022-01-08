ed6b4c62e4b62457ebe71229e8e01ba5a94d14a9
RosalieWessels <rosalie.wessels@yahoo.com>
2022-01-07 21:52:39 -0800

    Initial Commit

---
+++ Shared/Flag_Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_Guessing_GameApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Flag_Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
